b317a565f50e98bee7791194a5593227d84f9a7b
DOVYyvod <163853919+DOVYyvod@users.noreply.github.com>
2024-03-18 14:02:00 +0200

    Add files via upload

---
+++ JS/DaugiauApieMane.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const SideBar = document.querySelector('.SideBar')
+    SideBar .style. display = 'flex'
+}
+
+function hideSidebar(){
+    const SideBar = document.querySelector('.SideBar')
+    SideBar .style. display = 'none'
+}
